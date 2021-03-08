cb326924e394f2a65532985f1be320da702d500a
yenitamelia <yenitamelia2@gmail.com>
2021-03-08 08:23:48 -0800

    first commit

---
+++ local/config.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+return array(
+
+	'config' => array(
+		'base_url' => 'http://localhost/classroom_bookings/',
+		'log_threshold' => 1,
+		'index_page' => 'index.php',
+		'uri_protocol' => 'REQUEST_URI',
+	),
+
+	'database' => array(
+		'dsn' => '',
+		'hostname' => 'localhost',
+		'username' => 'admin',
+		'password' => 'admin123',
+		'database' => 'ppl',
+		'dbdriver' => 'pdo',
+		'subdriver' => 'mysql',
+	),
+
+);
